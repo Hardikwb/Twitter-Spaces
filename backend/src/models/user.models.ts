e7a29871e2635bd0cbf278be475f4a65579c667a
Hardikwb <hghardikgupta1@gmail.com>
2026-06-22 22:36:59 +0530

    Adding user model

---
+++ backend/src/models/user.models.ts
@@ -0,0 +1,22 @@
+import { getTRPCErrorFromUnknown } from "@trpc/server"
+import mongoose, { Schema } from "mongoose"
+
+const userSchema = new Schema({
+    username:{
+        type:String
+    },
+    email:{
+        type:String,
+        unique:true
+    },
+    activated:{
+        type:Boolean,
+        default:false
+    },
+    refreshToken:{
+        type:String,
+    },
+},{timestamps:true})
+
+const userModel = mongoose.model("user",userSchema)
+export default userModel
