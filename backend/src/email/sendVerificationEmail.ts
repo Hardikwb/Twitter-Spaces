d0d35ecb45a688c15494a89674ab51651dc1c1c3
Hardikwb <hghardikgupta1@gmail.com>
2026-06-23 22:43:54 +0530

    Adding Email and Code services

--- backend/src/email/sendVerificationEmail.ts
+++ backend/src/email/sendVerificationEmail.ts
@@ -1,26 +1,25 @@
-import { Resend } from "resend"
-import config from "../config/config.js"
+import { Resend } from "resend";
+import config from "../config/config.js";
 import VerificationEmailTemplate from "./VerificationEmailTemplate.js";
 
-const resend = new Resend(config.RESEND)
+const resend = new Resend(config.RESEND);
 
-const sendVerificationEmail = async(
-    email:string,
-    verifyCode:string,
-)=>{
-     try {
+const sendVerificationEmail = async (email: string, verifyCode: string) => {
+  try {
     await resend.emails.send({
       from: "Spaces <onboarding@resend.dev>",
       to: email,
       subject: "Verification Email from Spaces",
-      react: VerificationEmailTemplate({ email: email, verifyCode: verifyCode }),
+      react: VerificationEmailTemplate({
+        email: email,
+        verifyCode: verifyCode,
+      }),
     });
     return { success: true, message: "Verification email send successfully" };
-    } 
-  catch (emailError) {
+  } catch (emailError) {
     console.log(`Error while sending message:: ${emailError} `);
     return { success: false, message: "Fail to send email" };
   }
-}
+};
 
-export default sendVerificationEmail
+export default sendVerificationEmail;
