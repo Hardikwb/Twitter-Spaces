36c14b3b26461eaaa0cb71a9e53bb677d31e9317
Hardikwb <hghardikgupta1@gmail.com>
2026-06-23 19:07:02 +0530

    Updating existing services

--- backend/src/services/code.services.ts
+++ backend/src/services/code.services.ts
@@ -1,20 +1,16 @@
-import crypto from "node:crypto"
+import crypto from "node:crypto";
 import hashServices from "./hash.services.js";
 
-class CodeService{
-    generateCode(){
-        return crypto.randomInt(1000,9999);
-    }
-    
-    sendCode(){
+class CodeService {
+  generateCode() {
+    return crypto.randomInt(1000, 9999);
+  }
 
-    }
+  sendCode() {}
 
-    verifyCode(code:string,hashedCode:string){
-        return hashServices.hashData(code)===hashedCode
-    }
-
-
+  verifyCode(code: string, hashedCode: string) {
+    return hashServices.hashData(code) === hashedCode;
+  }
 }
-const codeService = new CodeService()
-export default codeService
+const codeService = new CodeService();
+export default codeService;
--- backend/src/services/token.service.ts
+++ backend/src/services/token.service.ts
@@ -1,18 +1,54 @@
-import jwt from "jsonwebtoken"
-import crypto from "node:crypto"
-import config from "../config/config.js"
+import jwt from "jsonwebtoken";
+import crypto from "node:crypto";
+import config from "../config/config.js";
+import type { ITokenResponse } from "../controllers/auth.controller.js";
+import userservice from "./user.services.js";
+import userModel from "../models/user.models.js";
 
-class TokenService{
-    generateTokens(payload:object){
-        const accessToken = jwt.sign(payload,config.ACCESS_TOKEN_SECRET!,{
-            expiresIn:"1h"
-        })
-        const refreshToken = jwt.sign(payload,config.REFRESH_TOKEN_SECRET!,{
-            expiresIn:"30d"
-        })
-        return {accessToken,refreshToken}
-    }
+class TokenService {
+  generateTokens(payload: object) {
+    const accessToken = jwt.sign(payload, config.ACCESS_TOKEN_SECRET!, {
+      expiresIn: "1m",
+    });
+    const refreshToken = jwt.sign(payload, config.REFRESH_TOKEN_SECRET!, {
+      expiresIn: "2m",
+    });
+    return { accessToken, refreshToken };
+  }
+
+  // async verifyAccessToken({accessToken}:{accessToken:string}){
+  async verifyAccessToken(accessToken: string) {
+    //    const userData:ITokenResponse = jwt.verify(accessToken,config.ACCESS_TOKEN_SECRET!)
+    const userData = jwt.verify(
+      accessToken,
+      config.ACCESS_TOKEN_SECRET!,
+    ) as ITokenResponse;
+    return userData;
+  }
+
+  async verifyRefreshToken(refreshToken: string) {
+    const userData = jwt.verify(
+      refreshToken,
+      config.REFRESH_TOKEN_SECRET!,
+    ) as ITokenResponse;
+    return userData;
+  }
+
+  async findRefreshToken(userId: string, refreshToken: string) {
+    const user = await userservice.findUser({
+      _id: userId,
+      refreshToken: refreshToken,
+    });
+    return user;
+  }
+
+  async updateRefreshToken(userId: string, refreshToken: string) {
+    return await userModel.updateOne(
+      { _id: userId },
+      { refreshToken: refreshToken },
+    );
+  }
 }
 
-const tokenService = new TokenService()
-export default tokenService
+const tokenService = new TokenService();
+export default tokenService;
