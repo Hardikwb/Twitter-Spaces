6a04069aef4a8a96ed70a994256e0fe1c897327e
Hardikwb <hghardikgupta1@gmail.com>
2026-06-23 22:41:29 +0530

    Updating routes and services

--- backend/src/route/auth.route.ts
+++ backend/src/route/auth.route.ts
@@ -1,9 +1,19 @@
-import express from "express"
-import authController from "../controllers/auth.controller.js"
+import express from "express";
+import authController from "../controllers/auth.controller.js";
+import { upload } from "../middleware/multer.js";
+import authMiddleWare from "../middleware/auth.middleware.js";
 
-const authRouter = express.Router()
+const authRouter = express.Router();
 
-authRouter.post('/send-otp',authController.sendCode)
-authRouter.post('/verify-otp',authController.verifyCode)
+authRouter.post("/send-otp", authController.sendCode);
+authRouter.post("/verify-otp", authController.verifyCode);
+authRouter.post(
+  "/activate",
+  upload.single("avatar"),
+  authMiddleWare,
+  authController.activate,
+);
+authRouter.get("/refresh", authController.refresh);
+authRouter.get("/logout",authMiddleWare, authController.logout);
 
-export default authRouter
+export default authRouter;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,30 +1,37 @@
-import express from "express"
-import cors from "cors"
-import config from "./config/config.js"
-import authRouter from "./route/auth.route.js"
-import connectDB from "./services/db.js"
+import express from "express";
+import cors from "cors";
+import config from "./config/config.js";
+import cookieParser from "cookie-parser";
+import authRouter from "./route/auth.route.js";
+import connectDB from "./services/db.js";
+const app = express();
+const PORT = config.PORT;
+const DOMAIN = config.DOMAIN;
 
-const app = express()
-const PORT = config.PORT
-const DOMAIN = config.DOMAIN
+connectDB();
 
-connectDB()
+const corsOptions = {
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void,
+  ) => {
+    if (!origin) return callback(null, true);
+    callback(null, true);
+  },
+  credentials: true,
+};
 
-const corsOptions={
-    // origin:['http://localhost:3000']
-    origin:'*'
-}
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
-app.use(cors(corsOptions))
-app.use(express.json())
+app.get("/", (req, res) => {
+  return res.send("Hello");
+});
 
-app.get('/',(req,res)=>{
-    return res.send("Hello")
-})
+app.use("/api/auth", authRouter);
 
-app.use('/api/auth', authRouter)
-
-
-app.listen(PORT,()=>{
-    console.log(`App listening on ${DOMAIN}:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`App listening on ${DOMAIN}:${PORT}`);
+});
